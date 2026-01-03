5c61bf0d0cca71202d816f6ef28c8e471449620a
Hume <73652606+HumeLinux@users.noreply.github.com>
2026-01-03 11:47:30 +0800

    2026.1.1

--- dynamic_memory_allocate2.c
+++ dynamic_memory_allocate2.c
@@ -38,10 +38,42 @@ void create2( char ** strPtr , int n )
 {
     for(int i = 0; i < n; i++)
         {
-            strPtr[]
+            strPtr[i] = (char *)malloc(n+1 * sizeof(char));
         }
 }
+
 void fill(char ** strPtr , int n)
 {
+        int rows = 2 * n + 1;  
+    int width = 2 * n + 1; 
     
+    for (int i = 0; i < rows; i++) {
+
+        for (int j = 0; j < width; j++) {
+            strPtr[i][j] = ' ';
+        }
+        
+        if (i == 0) {
+
+            strPtr[i][n] = 'X';
+        } else if (i < n) {
+ 
+            int left_pos = n - i;
+            int right_pos = n + i;
+            strPtr[i][left_pos] = '/';
+            strPtr[i][right_pos] = '\\'; 
+        } else if (i == n) {
+            strPtr[i][0] = 'X';
+            strPtr[i][width - 1] = 'X';
+        } else if (i < rows - 1) {
+            int j = rows - 1 - i;
+            int left_pos = n - j;
+            int right_pos = n + j;
+            strPtr[i][left_pos] = '\\'; 
+            strPtr[i][right_pos] = '/';
+        } else {
+            strPtr[i][n] = 'X';
+        }
+        strPtr[i][width] = '\0';
+    }
 }
